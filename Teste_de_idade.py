6683ef27efaaf6da3f067e348c64e8b3f66ec427
anthonyemvocacao <anthonybezerra388@gmail.com>
2026-03-09 11:49:42 -0300

    Upload
    digitação codigo

---
+++ Teste_de_idade.py
@@ -0,0 +1,18 @@
+'''
+
+
+'''
+
+print('15 anos')
+
+
+idade = int(input())
+
+if idade < 15:
+    print('o jovem é menor de idade')
+elif  idade >= 18:
+    print('o jovem é menor de idade')
+elif idade >= 64:
+    print('o jovem é adulto, é maior de idade')
+else:
+    print('o jovem é idoso, é maior de idade')
